8725a61c009df48c7f10850088632644ccbe4733
Vaibhav9520 <vaibhavsingh01080@gmail.com>
2024-09-09 23:45:41 +0530

    first commit

---
+++ ReverseString.java
@@ -0,0 +1,18 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        char[] arr = {'H', 'E', 'L', 'L', 'O'};
+        int n = arr.length;
+
+        char[] brr = new char[n]; 
+
+        // Reverse the array
+        for (int i = 0; i < n; i++) {
+            brr[i] = arr[n - i - 1]; 
+        }
+
+        // Print the reversed array
+        for (int i = 0; i < n; i++) {
+            System.out.print(brr[i] + " "); 
+        }
+    }
+}
