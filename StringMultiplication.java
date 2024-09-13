f41a96e7cf1c712163f1361ce4e2a86d24fd7b4f
Vaibhav9520 <vaibhavsingh01080@gmail.com>
2024-09-13 14:51:47 +0530

    update

---
+++ StringMultiplication.java
@@ -0,0 +1,13 @@
+public class StringMultiplication {
+    public static void main(String[] args) {
+        String str1 = "2";
+        String str2 = "3";
+
+        int num1 = Integer.parseInt(str1);
+        int num2 = Integer.parseInt(str2);
+
+        int result = num1 * num2;
+        System.out.println("Multipliction of String1 and String 2 is: " +result);
+
+    }
+}
