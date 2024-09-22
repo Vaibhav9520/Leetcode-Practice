fe4eacd49008cf28f1a1b2c1425c8921a63ab9bb
Vaibhav Singh <119606722+Vaibhav9520@users.noreply.github.com>
2024-09-22 22:20:32 +0530

    Update

--- ReverseString.java
+++ ReverseString.java
@@ -2,12 +2,12 @@ public class ReverseString {
     public static void main(String[] args) {
         char[] arr = {'H', 'E', 'L', 'L', 'O'};
         int n = arr.length;
-        int m;
+        
         
     
 
         char[] brr = new char[n]; 
-
+]
         // Reverse the array
         for (int i = 0; i < n; i++) {
             brr[i] = arr[n - i - 1]; 
